4e06e44c6f754a910c8d7849534ecee462eaa60a
heidechv <heidechv@rose-hulman.edu>
2018-01-16 19:53:10 -0500

    m8 finished

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by DELETING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Hailey Heidecker.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m6_mutation
 
@@ -65,13 +65,19 @@ def RETURN_delete_negatives(numbers):
       :type numbers: list
     where the list is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # DONE: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
     #             m6_mutation.run_test(...)
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+    new_numbers = []
+    for k in range(len(numbers)):
+        if numbers[k] >= 0:
+            new_numbers = new_numbers + [numbers[k]]
+
+    return new_numbers
 
 
 def run_test_MUTATE_delete_negatives():
@@ -123,8 +129,9 @@ def MUTATE_delete_negatives(numbers):
     The function must NOT return anything (other than the default None).
 
     Precondition: The argument is a list of numbers.
+    :type numbers: object
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
@@ -137,6 +144,15 @@ def MUTATE_delete_negatives(numbers):
     #       Take a stab at this problem,
     #       then see the solutions posted on Piazza.
     #       See those solutions even if your approach passes the tests.
+    num_negs = 0
+    for k in range(len(numbers)):
+        index = k - num_negs
+        if numbers[index] < 0:
+            numbers.remove(numbers[index])
+            num_negs = num_negs + 1
+
+
+
 
 
 # ----------------------------------------------------------------------
